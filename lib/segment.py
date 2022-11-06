c5873adb543371a3e18c11fc722db1eb8bfa0301
Wesly Giovano <weslygio@yahoo.com>
2022-11-06 18:22:40 +0700

    add segment checksum

--- lib/segment.py
+++ lib/segment.py
@@ -4,6 +4,19 @@ import struct
 SYN_FLAG = 0b01000000
 ACK_FLAG = 0b00001000
 FIN_FLAG = 0b10000000
+MAX_PAYLOAD_SIZE = 32756
+
+# -- Utility Function --
+def add_one_complement(m : int, n : int, size : int) -> int:
+    # m and n is size-bit integer
+    sum = m + n
+    max_val = 1 << size
+    if sum < max_val:
+        return sum
+    else:
+        # Guarantee: (m + n) < (2 * 2^size)
+        return (sum + 1) % max_val
+
 
 class SegmentFlag:
     def __init__(self, flag : bytes):
@@ -22,30 +35,45 @@ class Segment:
         self.seq_nb = 0
         self.ack_nb = 0
         self.flag = SegmentFlag(b'\x00')
-        self.checksum = 0
         self.payload = b''
+        self.payload_size = 0
+        self.checksum = 0
+        self.checksum = self.__calculate_checksum()
 
     def __str__(self):
-        # Optional, override this method for easier print(segmentA)
         output = ""
         output += f"{'Sequence number':24} | {self.seq_nb}\n"
         return output
 
     def __calculate_checksum(self) -> int:
-        # Checksum calculation with Internet Checksum
-        pass
+        # Checksum calculation with Internet Checksum (16-bit)
+        # against the whole segment
+        segment_bytes = self.get_bytes()
+        checksum = 0
 
+        for i in range(0, len(segment_bytes), 2):
+            checksum = add_one_complement(checksum, int.from_bytes(segment_bytes[i:i+2], byteorder='big'), size=16)
+        
+        checksum = ~checksum + (1 << 16)
+        return checksum
 
     # -- Setter --
     def set_header(self, header : dict):
         self.seq_nb = header['seq_nb']
         self.ack_nb = header['ack_nb']
+        self.checksum = 0
+        self.checksum = self.__calculate_checksum()
 
     def set_payload(self, payload : bytes):
         self.payload = payload
+        self.payload_size = len(payload)
+        self.checksum = 0
+        self.checksum = self.__calculate_checksum()
 
     def set_flag(self, flag_list : list):
         self.flag = SegmentFlag(sum(flag_list).to_bytes(1, byteorder='big'))
+        self.checksum = 0
+        self.checksum = self.__calculate_checksum()
 
 
     # -- Getter --
@@ -63,16 +91,17 @@ class Segment:
     # -- Marshalling --
     def set_from_bytes(self, src : bytes):
         # From pure bytes, unpack() and set into python variable
-        self.seq_nb, self.ack_nb, flag_byte, self.checksum, self.payload = struct.unpack('!IIsxH32756s', src)
+        self.payload_size = len(src) - 12
+        self.seq_nb, self.ack_nb, flag_byte, self.checksum, self.payload = struct.unpack(f'!IIsxH{self.payload_size}s', src)
         self.flag = SegmentFlag(flag_byte)
 
     def get_bytes(self) -> bytes:
         # Convert this object to pure bytes
-        return struct.pack('!IIsxH32756s', self.seq_nb, self.ack_nb, self.flag.get_flag_bytes(),
-                                           self.checksum, self.payload)
+        return struct.pack(f'!IIsxH{self.payload_size}s', self.seq_nb, self.ack_nb, self.flag.get_flag_bytes(),
+                                                          self.checksum, self.payload)
 
 
     # -- Checksum --
     def valid_checksum(self) -> bool:
-        # Use __calculate_checksum() and check integrity of this object
-        pass
+        # Check integrity of this object
+        return self.__calculate_checksum() == 0x0000
